b0d412b005dfe13c861b6a1ec8307c390487747f
Kacper Konecki <ka.konecki@student.uw.edu.pl>
2018-04-16 14:23:48 +0200

    quiz entity addded

---
+++ app/src/main/java/com/gaspard/quizer/QuizEntity.java
@@ -0,0 +1,90 @@
+package com.gaspard.quizer;
+
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+import android.support.annotation.NonNull;
+
+@Entity(tableName = "quizzes")
+public class QuizEntity {
+
+    @NonNull
+    public int getId() {
+        return id;
+    }
+
+    public void setId(@NonNull int id) {
+        this.id = id;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getQstCnt() {
+        return qstCnt;
+    }
+
+    public void setQstCnt(int qstCnt) {
+        this.qstCnt = qstCnt;
+    }
+
+    public int getLastScore() {
+        return lastScore;
+    }
+
+    public void setLastScore(int lastScore) {
+        this.lastScore = lastScore;
+    }
+
+    public int getLastQuestion() {
+        return lastQuestion;
+    }
+
+    public void setLastQuestion(int lastQuestion) {
+        this.lastQuestion = lastQuestion;
+    }
+
+    @NonNull
+    @PrimaryKey
+    @ColumnInfo(name = "id")
+    private int id;
+
+    @ColumnInfo(name = "url")
+    private String url;
+
+    @ColumnInfo(name = "title")
+    private String title;
+
+    /*TODO remove this field and add SQL function*/
+    @ColumnInfo(name = "qst_cnt")
+    private int qstCnt;
+
+    @ColumnInfo(name = "last_score")
+    private int lastScore;
+
+    @ColumnInfo(name = "last_question")
+    private int lastQuestion;
+
+
+    public QuizEntity(@NonNull int id, String url, String title, int qstCnt, int lastScore, int lastQuestion) {
+        this.id = id;
+        this.url = url;
+        this.title = title;
+        this.qstCnt = qstCnt;
+        this.lastScore = lastScore;
+        this.lastQuestion = lastQuestion;
+    }
+}
